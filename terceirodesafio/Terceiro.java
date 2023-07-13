f66969d902b2b4c29b92ba11df27fe7340a84886
Thiago <thiago.spereira02@gmail.com>
2023-07-13 18:04:08 -0300

    solving challenge 3

--- terceirodesafio/Terceiro.java
+++ terceirodesafio/Terceiro.java
@@ -1,5 +1,32 @@
 package terceirodesafio;
 
+import java.util.Scanner;
+
 public class Terceiro {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(values[i] - values[j]) == k) {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+
+        scanner.close();
+    }
 }
